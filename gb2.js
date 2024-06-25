ffe39ac410c65faf90549791d34d4c1358a7eace
uknow <wangyahuiuknow@gmail.com>
2024-06-25 16:58:14 +0800

    Initial commit

--- gb2.js
+++ gb2.js
@@ -24,11 +24,66 @@ function sendNotification(title, subtitle, message) {
     $notify(title, subtitle, message);
 }
   
-  
-data[0].status = 4;
-data[0].ticketPool = 200;
+var newItem = {
+    "saleMode": null,
+    "openPerson": null,
+    "status": 4,
+    "ticketPool": 200,
+    "currentDate": null,
+    "seasonType": null,
+    "hallType": 4,
+    "hallTicketPoolVOS": null,
+    "today": 0,
+    "hallId": 1,
+    "name": null,
+    "images": "",
+    "imagesPc": null,
+    "content": null,
+    "ruleContent": null,
+    "closeContent": null,
+    "scheduleTicketPoolVOS": null,
+    "hallScheduleId": 3,
+    "priceId": 8,
+    "priceName": "免费参观凭证",
+    "price": "0.00",
+    "ageRange": "0-110",
+    "ageMin": "0",
+    "ageMax": "110",
+    "notRecommendPrice": false,
+    "child": false,
+    "tourEscort": false,
+    "adult": true,
+    "recommendAgeMin": "0",
+    "recommendAgeMax": "110",
+    "certificateVOS": [{
+        "id": 1,
+        "name": "身份证"
+    }, {
+        "id": 2,
+        "name": "护照"
+    }, {
+        "id": 3,
+        "name": "港澳居民来往内地通行证"
+    }, {
+        "id": 4,
+        "name": "台湾居民来往大陆通行证"
+    }, {
+        "id": 6,
+        "name": "外国人永久居留身份证"
+    }, {
+        "id": 7,
+        "name": "港澳台居民居住证"
+    }],
+    "ticketSetAmountFlag": false,
+    "document": null,
+    "showDocument": 0,
+    "showOrder": 2,
+    "priceNameForCalendar": "免费参观凭证0.00元/张",
+    "introduction": "基本陈列展免费参观凭证",
+    "student": false
+};
+data[0] = newItem;
 obj.data = data
-
 // 发送一个示例通知
 sendNotification("第二步", "获取预约信息", JSON.stringify(obj));
 
